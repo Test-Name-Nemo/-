4fe86a069e99c1cd57e1a3bc2025977aa827c48f
Dmitriy <dmitri-rt@ya.ru>
2024-05-06 21:48:19 +0300

    next_1

--- insert.sql
+++ insert.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 -- вариант 1
 
 INSERT INTO genre (name_genre) VALUES ('Славянский фолк');
@@ -44,39 +44,4 @@ INSERT INTO collections_track (id_collection, id_track) VALUES
 ('2', '2')
 ('3', '2')
 ('4', '2')
-=======
-INSERT INTO trach (title_track, duration_track) VALUES (Отшумели летние дожди, 188);
-INSERT INTO trach (title_track, duration_track) VALUES (Серые глаза, 204);
-INSERT INTO trach (title_track, duration_track) VALUES (Набат, 241);
-INSERT INTO trach (title_track, duration_track) VALUES (Русь, 202);
-INSERT INTO trach (title_track, duration_track) VALUES (Сибирь, );
-INSERT INTO trach (title_track, duration_track) VALUES (Вечерком, 189);
-INSERT INTO trach (title_track, duration_track) VALUES (Ясный сокол, 247);
-INSERT INTO trach (title_track, duration_track) VALUES (Купола, 320);
-INSERT INTO trach (title_track, duration_track) VALUES ();
 
-
-INSERT INTO artist (nick_name) VALUES (Николай Емелин);
-INSERT INTO artist (nick_name, names, surname) VALUES (Шура);
-INSERT INTO artist (nick_name, names, surname) VALUES (Ирина Салтыкова);
-INSERT INTO artist (nick_name, names, surname) VALUES (Круг);
-INSERT INTO artist (nick_name, names, surname) VALUES ();
-INSERT INTO artist (nick_name, names, surname) VALUES ();
-
-
-INSERT INTO genre (name_genre) VALUES (Славянский фолк);
-INSERT INTO name_genre (name_genre) VALUES (Шансон);
-INSERT INTO name_genre (name_genre) VALUES (Поп);
-INSERT INTO name_genre (name_genre) VALUES (Евродэнс);
-INSERT INTO name_genre (name_genre) VALUES ();
-
-INSERT INTO album (name_genre, year_album) VALUES 
-(Хочу куда нибудь свалить, 2020),
-(ЗавеД, 2018),
-(Живая струна, 2010)
-(Подруга, 2017)
-
-INSERT INTO name_genre (name_genre) VALUES ();
-
-
->>>>>>> b78eba79dae896b4a824a2ba51555af3d240b1a8
